6a086eb786a1a70aaf631bf67e71a5638357593d
linxh <linxh@6vbuy.com>
2019-01-28 18:32:07 +0800

    增加 LumenToolsServiceProvider

---
+++ src/LumenToolsServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Fxiao\LumenTools;
+
+use Illuminate\Support\ServiceProvider;
+
+class LumenToolsServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        if (env('APP_DEBUG', false) && env('DEV_HELPERS', false)) {
+            $this->registerRoutes();
+        }
+    }
+
+    public function registerRoutes()
+    {
+        $api = app('Dingo\Api\Routing\Router');
+
+        $api->get('/dev-helpers', [
+            'as' => 'helpers.index',
+            'uses' => '\Fxiao\LumenTools\HelpersController@index',
+        ]);
+
+        $api->post('/dev-helpers', [
+            'as' => 'helpers.store',
+            'uses' => '\Fxiao\LumenTools\HelpersController@store',
+        ]);
+        
+    }
+}
